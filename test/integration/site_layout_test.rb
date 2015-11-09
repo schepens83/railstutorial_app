6bd2f89f36589b89ad1fa525f238bf9cc2f6a4e7
schepens83 <schepens@gmail.com>
2015-11-09 12:48:00 +0100

    add signup page test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,4 +11,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 		assert_select "a[href=?]", contact_path
 	end
 
+	test "signup page" do
+		get signup_path
+		assert_template 'users/new'
+		assert_select "title", full_title("Sign up")
+	end
 end
