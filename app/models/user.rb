6c1b65d180690e67746b8865b0d567102103bf74
schepens83 <schepens@gmail.com>
2015-11-24 15:50:17 +0100

    adds logging in via sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false }
 	validates :password, presence: true, length: { minimum: 6 }
 	has_secure_password
+
+	# Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
